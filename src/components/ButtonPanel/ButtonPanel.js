102f04c6f9f210ed6927d0721f4a8566b1f24d9c
Anthony <anthonyviera4@gmail.com>
2021-01-18 19:13:26 -0600

    Creating buttons in button panel class is now handled in a modular way.

--- src/components/ButtonPanel/ButtonPanel.js
+++ src/components/ButtonPanel/ButtonPanel.js
@@ -3,60 +3,151 @@ import Button from './Button/Button';
 import styles from './buttonPanel.module.css';
 
 const buttonPanel = props => {
+    let buttonRow = [];
+    let buttonRowId = 0;
+    const buttonRows = [];
+    const buttons = [
+        {
+            name: "AC",
+            type: "Clear",
+            clicked: props.clear
+        },
+        {
+            name: "+/-",
+            type: "Number",
+            clicked: props.negative
+        },
+        {
+            name: "%",
+            type: "Number",
+            clicked: props.percent
+        },
+        {
+            name: "÷",
+            type: "Symbol",
+            clicked: props.divide
+        },
+        {
+            name: "7",
+            type: "Number",
+            clicked: props.numClicked
+        },
+        {
+            name: "8",
+            type: "Number",
+            clicked: props.numClicked
+        },
+        {
+            name: "9",
+            type: "Number",
+            clicked: props.numClicked
+        },
+        {
+            name: "x",
+            type: "Symbol",
+            clicked: props.multiplied
+        },
+        {
+            name: "4",
+            type: "Number",
+            clicked: props.numClicked
+        },
+        {
+            name: "5",
+            type: "Number",
+            clicked: props.numClicked
+        },
+        {
+            name: "6",
+            type: "Number",
+            clicked: props.numClicked
+        },
+        {
+            name: "-",
+            type: "Symbol",
+            clicked: props.multiplied
+        },
+        {
+            name: "1",
+            type: "Number",
+            clicked: props.numClicked
+        },
+        {
+            name: "2",
+            type: "Number",
+            clicked: props.numClicked
+        },
+        {
+            name: "3",
+            type: "Number",
+            clicked: props.numClicked
+        },
+        {
+            name: "+",
+            type: "Symbol",
+            clicked: props.multiplied
+        },
+        {
+            name: "0",
+            type: "Number",
+            clicked: props.numClicked
+        },
+        {
+            name: ".",
+            type: "Number",
+            clicked: props.decimalClicked
+        },
+        {
+            name: "=",
+            type: "Symbol",
+            clicked: props.equals
+        }
+    ];
+
+    const isSymbol = (btn) => {
+        return btn === '÷' ||
+            btn === 'x' ||
+            btn === '-' ||
+            btn === '+' ||
+            btn === '=';
+    }
+
+    buttons.forEach(btn => {
+        let newButton = (
+            <Button
+                key={btn.name}
+                btnType={btn.type}
+                clicked={btn.clicked}>
+                {btn.name}
+            </Button>
+        );
+        if (btn.name === "0") {
+            newButton = (
+                <Button
+                    key={btn.name}
+                    btnType={btn.type}
+                    clicked={btn.clicked}
+                    style={{ 'flexGrow': 2 }}>
+                    {btn.name}
+                </Button>
+            );
+        }
+        buttonRow.push(newButton);
+        if (isSymbol(btn.name)) {
+            buttonRows.push(
+                <div
+                    key={"button-row" + (buttonRowId + 1).toString()}
+                    className={styles.Row}>
+                    {buttonRow}
+                </div>);
+            buttonRow = [];
+            buttonRowId += 1;
+        }
+    });
+
     return (
         <div className={styles.ButtonPanel}>
-            <div className={styles.Row}>
-                <Button 
-                    btnType="Clear"
-                    clicked={props.clear}>AC</Button>
-                <Button btnType="Number">+/-</Button>
-                <Button btnType="Number">%</Button>
-                <Button btnType="Symbol">÷</Button>
-            </div>
-            <div className={styles.Row}>
-                <Button 
-                    btnType="Number" 
-                    clicked={props.numClicked}>7</Button>
-                <Button 
-                    btnType="Number" 
-                    clicked={props.numClicked}>8</Button>
-                <Button 
-                    btnType="Number" 
-                    clicked={props.numClicked}>9</Button>
-                <Button btnType="Symbol">x</Button>
-            </div>
-            <div className={styles.Row}>
-                <Button 
-                    btnType="Number" 
-                    clicked={props.numClicked}>4</Button>
-                <Button 
-                    btnType="Number" 
-                    clicked={props.numClicked}>5</Button>
-                <Button 
-                    btnType="Number" 
-                    clicked={props.numClicked}>6</Button>
-                <Button btnType="Symbol">-</Button>
-            </div>
-            <div className={styles.Row}>
-                <Button 
-                    btnType="Number" 
-                    clicked={props.numClicked}>1</Button>
-                <Button 
-                    btnType="Number" 
-                    clicked={props.numClicked}>2</Button>
-                <Button 
-                    btnType="Number" 
-                    clicked={props.numClicked}>3</Button>
-                <Button btnType="Symbol">+</Button>
-            </div>
-            <div className={styles.Row}>
-                <Button 
-                    btnType="Number"
-                    clicked={props.numClicked} 
-                    style={{'flexGrow': '2'}}>0</Button>
-                <Button btnType="Number">.</Button>
-                <Button btnType="Symbol">=</Button>
-            </div>
+            {buttonRows}
         </div>
     );
 }
