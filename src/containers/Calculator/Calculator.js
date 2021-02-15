35638ab27e6abb24c17462c612f88e0a158e41ed
Anthony9698 <anthonyviera4@gmail.com>
2021-02-14 21:42:35 -0600

    Added functionality for both multiply and divide.

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ButtonPanel from '../../components/ButtonPanel/ButtonPanel';
 import Display from '../../components/Display/Display';
 
 class Calculator extends Component {
     state = {
+        currVal: null,
         displayVal: null,
         equationIndex: 0,
-        equation: [] 
+        equation: []
     }
 
     operations = {
@@ -16,65 +17,52 @@ class Calculator extends Component {
         'x': (x, y) => x * y
     }
 
-    hasOperator = equation => {
-        for (let x = 0; x < equation.length; x++) {
-            if (equation[x] === '+' || equation[x] === '-' || equation[x] === '÷' || equation[x] === 'x') {
-                return true;
-            }
-        }
-        return false;
+    isOperator = symbol => {
+        return symbol === '+' || symbol === '-' || symbol === '÷' || symbol === 'x';
     }
 
     operationHelper = operator => {
-        let equation = this.state.equation;
-        let displayVal = this.state.displayVal;
+        let newEquation = [...this.state.equation];
+        let currVal = this.state.currVal;
 
-        if (this.hasOperator(equation) && equation.length > 2) {
-            let operand1 = parseInt(equation[0]);
-            let operand2 = parseInt(equation[2]);
-            let op = equation[1];
-            let calculation = this.operations[op](operand1, operand2);
-            equation = [];
-            equation.push(calculation);
-            equation.push(operator)
-            this.setState({equation: equation, displayVal: equation[0]});
-        } 
-        else if (equation.length === 0 ) {
-            if (!displayVal) { 
-                equation.push('0', operator); 
-                this.setState({displayVal: '0'}); 
+        if (currVal) {
+            if (newEquation.length === 2) {
+                let operand1 = parseInt(newEquation[0]);
+                let operand2 = parseInt(currVal);
+                let op = newEquation[1];
+                let calculation = this.operations[op](operand1, operand2);
+                if (calculation === Infinity) { calculation = 0; }
+                newEquation[0] = calculation;
+                newEquation[1] = operator;
+                this.setState({ equation: newEquation, displayVal: calculation, currVal: null });
             }
-            else { equation.push(displayVal, operator); }
-        } 
-        else {
-            equation[equation.length-1] = operator;
+            else {
+                this.setState({ equation: [...newEquation, currVal, operator], currVal: null });
+            }
         }
-
-        this.setState({equation: equation});
+        else if (!currVal && !this.isOperator(newEquation[newEquation.length - 1])) {
+            this.setState({ currVal: null, equation: [...newEquation, 0, operator] });
+        }
+        else {
+            newEquation[newEquation.length - 1] = operator;
+            this.setState({ equation: newEquation });
+        }
     }
 
     numClickedHandler = event => {
-        let currVal = this.state.displayVal;
-        let equation = this.state.equation;
+        let currVal = this.state.currVal;
         if (currVal === null) { currVal = event.target.value; }
-        else if (this.hasOperator(equation) && equation.length < 3) {
-            currVal = event.target.value;
-            equation.push(currVal);
-        }
-        else {
-            currVal += event.target.value;
-        }
-        this.setState({displayVal: currVal});
-        if (equation[2]) {
-            equation[2] = currVal;
-        }
+        else if (currVal !== 0) { currVal += event.target.value; }
+        this.setState({ currVal: currVal, displayVal: currVal });
     }
 
-    addHandler = () => this.operationHelper('+')
-    subHandler = () => this.operationHelper('-')
+    addHandler = () => this.operationHelper('+');
+    subHandler = () => this.operationHelper('-');
+    multHandler = () => this.operationHelper('x');
+    divHandler = () => this.operationHelper('÷');
 
     clearHandler = () => {
-        this.setState({displayVal: null, equation: [], equationIndex: 0});
+        this.setState({ displayVal: null, equation: [], equationIndex: 0 });
     }
 
     render() {
@@ -82,10 +70,12 @@ class Calculator extends Component {
             <div>
                 <Display value={this.state.displayVal === null ? '0' : this.state.displayVal} />
                 <ButtonPanel
-                    clearText={this.state.displayVal === null ? 'AC' : 'C'} 
+                    clearText={this.state.displayVal === null ? 'AC' : 'C'}
                     numClicked={this.numClickedHandler}
                     add={this.addHandler}
                     subtract={this.subHandler}
+                    multiply={this.multHandler}
+                    divide={this.divHandler}
                     clear={this.clearHandler} />
             </div>
         );
